13afd065120bb7ef1840b9848abdac10dbe4410e
Jhoan Gutierrez <jhoangutierrezc@gmail.com>
2024-04-23 13:28:28 -0500

    bnv, calls, ss & app icon - name

--- taller_whatsapp/lib/Screens/status_screen.dart
+++ taller_whatsapp/lib/Screens/status_screen.dart
@@ -1,95 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:taller_whatsapp/AppBars/AppBar.dart';
+import 'package:taller_whatsapp/Widgets/appbar.dart';
 
-class EstadosPage extends StatelessWidget {
-  final List<Estado> estados = [
-    Estado(
+class status_screen extends StatelessWidget {
+  status_screen({super.key});
+  final List<status> estados = [
+    status(
       nombre: 'Juan Pérez',
       tiempo: 'hace 30 minutos',
       imagen: 'assets/img/abogado.png',
       contenido: 'Trabajando duro en mi caso legal.',
     ),
-     Estado(
+    status(
       nombre: 'Juan Pérez',
       tiempo: 'hace 30 minutos',
       imagen: 'assets/img/abogado.png',
       contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
+    ),
+    status(
       nombre: 'Juan Pérez',
       tiempo: 'hace 30 minutos',
       imagen: 'assets/img/abogado.png',
       contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
+    ),
+    status(
       nombre: 'Juan Pérez',
       tiempo: 'hace 30 minutos',
       imagen: 'assets/img/abogado.png',
       contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
+    ),
+    status(
       nombre: 'Juan Pérez',
       tiempo: 'hace 30 minutos',
       imagen: 'assets/img/abogado.png',
       contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
+    ),
+    status(
       nombre: 'Juan Pérez',
       tiempo: 'hace 30 minutos',
       imagen: 'assets/img/abogado.png',
       contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
+    ),
+    status(
       nombre: 'Juan Pérez',
       tiempo: 'hace 30 minutos',
       imagen: 'assets/img/abogado.png',
       contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
+    ),
+    status(
       nombre: 'Juan Pérez',
       tiempo: 'hace 30 minutos',
       imagen: 'assets/img/abogado.png',
       contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
-      nombre: 'Juan Pérez',
-      tiempo: 'hace 30 minutos',
-      imagen: 'assets/img/abogado.png',
-      contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
-      nombre: 'Juan Pérez',
-      tiempo: 'hace 30 minutos',
-      imagen: 'assets/img/abogado.png',
-      contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
-      nombre: 'Juan Pérez',
-      tiempo: 'hace 30 minutos',
-      imagen: 'assets/img/abogado.png',
-      contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
-      nombre: 'Juan Pérez',
-      tiempo: 'hace 30 minutos',
-      imagen: 'assets/img/abogado.png',
-      contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
-      nombre: 'Juan Pérez',
-      tiempo: 'hace 30 minutos',
-      imagen: 'assets/img/abogado.png',
-      contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
-      nombre: 'Juan Pérez',
-      tiempo: 'hace 30 minutos',
-      imagen: 'assets/img/abogado.png',
-      contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
-      nombre: 'Juan Pérez',
-      tiempo: 'hace 30 minutos',
-      imagen: 'assets/img/abogado.png',
-      contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
-      nombre: 'Juan Pérez',
-      tiempo: 'hace 30 minutos',
-      imagen: 'assets/img/abogado.png',
-      contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
-      nombre: 'Juan Pérez',
-      tiempo: 'hace 30 minutos',
-      imagen: 'assets/img/abogado.png',
-      contenido: 'Trabajando duro en mi caso legal.',
-    ), Estado(
+    ),
+    status(
       nombre: 'Juan Pérez',
       tiempo: 'hace 30 minutos',
       imagen: 'assets/img/abogado.png',
@@ -99,7 +62,6 @@ class EstadosPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -109,28 +71,26 @@ class EstadosPage extends StatelessWidget {
             SliverAppBar(
               pinned: false,
               floating: true,
-              expandedHeight: size.height * 0.0, 
-              bottom: PreferredSize(
-                preferredSize: Size.fromHeight(0.0), 
-                child: AppBarWhatsApp(pantalla: Pantalla.Comunidades),
+              expandedHeight: size.height * 0.0,
+              bottom: const PreferredSize(
+                preferredSize: Size.fromHeight(0.0),
+                child: AppBarWhatsApp(pantalla: Pantalla.Novedades),
               ),
             ),
           ];
         },
         body: ListView.builder(
-          padding: EdgeInsets.only(top: size.height * 0.0), 
+          padding: EdgeInsets.only(top: size.height * 0.0),
           itemCount: estados.length,
           itemBuilder: (context, index) {
-            
             return ListTile(
               leading: CircleAvatar(
+                radius: size.height * 0.03,
                 backgroundImage: AssetImage(estados[index].imagen),
               ),
               title: Text(estados[index].nombre),
               subtitle: Text(estados[index].tiempo),
-              onTap: () {
-                
-              },
+              onTap: () {},
             );
           },
         ),
@@ -139,13 +99,13 @@ class EstadosPage extends StatelessWidget {
   }
 }
 
-class Estado {
+class status {
   final String nombre;
   final String tiempo;
   final String imagen;
   final String contenido;
 
-  Estado({
+  status({
     required this.nombre,
     required this.tiempo,
     required this.imagen,
